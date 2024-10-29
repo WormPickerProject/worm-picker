ee5ada978d24a760a6e47a09cca7a0d1fc193655
lkaising <lkaising@outlook.com>
2024-10-29 00:14:45 -0400

    Updated task generator to use polymorphism.

--- worm_picker_core/include/worm_picker_core/tasks/generation/generate_hotel_place_plate_task.hpp
+++ worm_picker_core/include/worm_picker_core/tasks/generation/generate_hotel_place_plate_task.hpp
@@ -3,15 +3,15 @@
 #ifndef GENERATE_HOTEL_PLACE_PLATE_TASK_HPP
 #define GENERATE_HOTEL_PLACE_PLATE_TASK_HPP
 
-#include "worm_picker_core/tasks/task_data_structure.hpp"
+#include "worm_picker_core/tasks/generation/base_task_generator.hpp"
 
-class GenerateHotelPlacePlateTask
+class GenerateHotelPlacePlateTask : public BaseTaskGenerator
 {
 public:
     GenerateHotelPlacePlateTask(const std::unordered_map<std::string, HotelData>& hotel_map);
 
-    void generateTasks();
-    const std::map<std::string, TaskData>& getTaskDataMap() const;
+    void generateTasks() override;
+    const std::map<std::string, TaskData>& getTaskDataMap() const override;
 
 private:
     const std::unordered_map<std::string, HotelData>& hotel_data_map_;
