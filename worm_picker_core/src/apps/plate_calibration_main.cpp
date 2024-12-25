ccdb1b278229702163464e10499b1553470240df
lkaising <lkaising@outlook.com>
2024-12-25 15:30:26 -0500

    Uncomment main function implementations in plate calibration and worm picker applications: Restore main function logic for initializing nodes, executing multi-threaded executors, and shutting down properly in plate_calibration_main.cpp and worm_picker_main.cpp. Remove commented-out code in corresponding source files for clarity.

--- worm_picker_core/src/apps/plate_calibration_main.cpp
+++ worm_picker_core/src/apps/plate_calibration_main.cpp
@@ -5,17 +5,17 @@
 
 #include "worm_picker_core/system/nodes/plate_calibration.hpp"
 
-// int main(int argc, char **argv) 
-// {
-//     rclcpp::init(argc, argv);
-//     auto options = rclcpp::NodeOptions().automatically_declare_parameters_from_overrides(true);
-//     auto plate_calibration = std::make_shared<PlateCalibration>(options);
+int main(int argc, char **argv) 
+{
+    rclcpp::init(argc, argv);
+    auto options = rclcpp::NodeOptions().automatically_declare_parameters_from_overrides(true);
+    auto plate_calibration = std::make_shared<PlateCalibration>(options);
 
-//     rclcpp::executors::MultiThreadedExecutor executor;
-//     executor.add_node(plate_calibration->getNodeBase());
-//     executor.spin();
-//     executor.remove_node(plate_calibration->getNodeBase());
+    rclcpp::executors::MultiThreadedExecutor executor;
+    executor.add_node(plate_calibration->getNodeBase());
+    executor.spin();
+    executor.remove_node(plate_calibration->getNodeBase());
 
-//     rclcpp::shutdown();
-//     return 0;
-// }
+    rclcpp::shutdown();
+    return 0;
+}
