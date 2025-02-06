ba72ca8aeb9c5dd77ab279ef4c8ebefd93b44749
lkaising <lkaising@outlook.com>
2025-02-06 18:10:52 -0500

    Add move_to_circle_data stage implementation to support circular motion

--- worm_picker_core/include/worm_picker_core/core/tasks/stages/move_to_circle_data.hpp
+++ worm_picker_core/include/worm_picker_core/core/tasks/stages/move_to_circle_data.hpp
@@ -3,3 +3,7 @@
 // Copyright (c) 2025
 // SPDX-License-Identifier: Apache-2.0
 
+#pragma once
+
+#include "worm_picker_core/core/tasks/stages/movement_data_base.hpp"
+
--- worm_picker_core/src/core/tasks/stages/move_to_circle_data.cpp
+++ worm_picker_core/src/core/tasks/stages/move_to_circle_data.cpp
@@ -3,3 +3,8 @@
 // Copyright (c) 2025
 // SPDX-License-Identifier: Apache-2.0
 
+#include <moveit/task_constructor/stages.h>
+#include <moveit/task_constructor/solvers.h>
+#include "worm_picker_core/utils/parameter_utils.hpp"
+#include "worm_picker_core/core/tasks/stages/move_to_circle_data.hpp"
+
