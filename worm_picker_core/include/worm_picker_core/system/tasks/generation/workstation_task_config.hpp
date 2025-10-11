5499db5fdb4109030a64af8e8cb0f1d6fcdd3acd
serge <serge.alhalbi@gmail.com>
2025-10-11 17:06:47 -0400

    Set the hover pick offset z a bit higher to work with 10 cm plate

--- worm_picker_core/include/worm_picker_core/system/tasks/generation/workstation_task_config.hpp
+++ worm_picker_core/include/worm_picker_core/system/tasks/generation/workstation_task_config.hpp
@@ -24,7 +24,7 @@ struct OffsetXYZ {
 namespace offset {
     static constexpr OffsetXYZ PICK {-0.045, 0.000, 0.0175};
     static constexpr OffsetXYZ PLACE{ 0.000, 0.000, 0.0175};
-    static constexpr OffsetXYZ HOVER{ 0.000, 0.000, 0.0440};
+    static constexpr OffsetXYZ HOVER{ 0.000, 0.000, 0.0450};
     static constexpr OffsetXYZ POINT{ 0.000, 0.000, 0.0100};
 }
 
